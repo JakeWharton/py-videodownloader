2158604d997fffdabd7229dd998e8742047fe9bc
JakeWharton <jakewharton@gmail.com>
2010-03-05 20:05:21 -0500

    Turn main file into a script.

--- video-downloader/video-downloader.py
+++ video-downloader/video-downloader.py
@@ -18,11 +18,44 @@ Public License along with py-video-downloader.  If not, see
 <http://www.gnu.org/licenses/>.
 '''
 
+from optparse import OptionParser, OptionGroup
 from providers import YouTube, Vimeo
 
-print 'video-downloader-1.0.0pre - by Jake Wharton <jakewharton@gmail.com>'
-print
-
 if __name__ == '__main__':
-  YouTube('tgbNymZ7vqY', debug=True).run()
-  Vimeo('5720832', debug=True).run()
+  DEFAULT_VIMEO = []
+  DEFAULT_YOUTUBE = []
+
+  DEFAULT_DEBUG = False
+
+  version =  '''
+  video-downloader-1.0.0pre - by Jake Wharton <jakewharton@gmail.com>
+  '''
+  parser = OptionParser(usage="Usage: %prog [options] file1 [... fileN]", version=version)
+
+  group = OptionGroup(parser, 'Video Provider Switches')
+  group.add_option('-V', dest='vimeo_id', action='append', default=DEFAULT_VIMEO, help='Download video from YouTube.')
+  group.add_option('-Y', dest='youtube_id', action='append', default=DEFAULT_YOUTUBE, help='Download video from Vimeo.')
+  parser.add_option_group(group)
+
+  group = OptionGroup(parser, "Display Options")
+  group.add_option('--debug', dest='is_debug', action='store_true', default=DEFAULT_DEBUG, help='Turn on debugging output on.')
+  parser.add_option_group(group)
+
+  options, args = parser.parse_args()
+
+  if len(args) > 0:
+    raise ValueError('All video IDs must be specified with a provider switch.')
+  if len(options.vimeo_id) == 0 and len(options.youtube_id) == 0:
+    raise ValueError('You must specify at least one video to download.')
+
+  for video in options.vimeo_id:
+    v = Vimeo(video, debug=options.is_debug)
+    print 'Downloading %s ("%s")...' % (video, v.get_title())
+    v.run()
+  for video in options.youtube_id:
+    v = YouTube(video, debug=options.is_debug)
+    print 'Downloading %s ("%s")...' % (video, v.get_title())
+    v.run()
+
+  print
+  print 'Done.'
