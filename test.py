6cf3446ab7e4f419707b1d0acea0f7ac2a39bd74
JakeWharton <jakewharton@gmail.com>
2010-05-18 19:39:52 -0400

    Expand test suites.

--- test.py
+++ test.py
@@ -28,31 +28,65 @@ from videodownloader.providers import Vimeo, YouTube
 
 
 class VimeoTests(TestCase):
-    def test_get_formats(self):
-        video = Vimeo('5720832')
-        self.assertTrue(video.formats == set(['sd', 'hd']))
+    video = Vimeo('5720832')
 
     def test_best_format(self):
-        video = Vimeo('5720832')
-        self.assertEqual(video._get_best_format(), 'hd')
+        self.assertEqual(VimeoTests.video._get_best_format(), 'hd')
+
+    def test_comments(self):
+        self.assertNotEqual(VimeoTests.video.comments, None)
+
+    def test_duration(self):
+        self.assertEqual(VimeoTests.video.duration, '299')
+
+    def test_formats(self):
+        self.assertEqual(VimeoTests.video.formats, set(['sd', 'hd']))
+
+    def test_height(self):
+        self.assertEqual(VimeoTests.video.height, '720')
+
+    def test_likes(self):
+        self.assertNotEqual(VimeoTests.video.likes, None)
+
+    def test_plays(self):
+        self.assertNotEqual(VimeoTests.video.plays, None)
+
+    def test_request_expiration(self):
+        self.assertNotEqual(VimeoTests.video.request_expiration, None)
+
+    def test_request_signature(self):
+        self.assertNotEqual(VimeoTests.video.request_signature, None)
+
+    def test_thumbnail(self):
+        self.assertNotEqual(VimeoTests.video.thumbnail, None)
 
     def test_title(self):
-        video = Vimeo('5720832')
-        self.assertEqual(video.title, 'Brand New - Jesus (Daisy sessions)')
+        self.assertEqual(VimeoTests.video.title, 'Brand New - Jesus (Daisy sessions)')
+
+    def test_uploader(self):
+        self.assertNotEqual(VimeoTests.video.uploader, None)
+
+    def test_width(self):
+        self.assertNotEqual(VimeoTests.video.width, None)
 
 
 class YouTubeTests(TestCase):
-    def test_get_formats(self):
-        video = YouTube('tgbNymZ7vqY')
-        self.assertTrue(video.formats == set(['5', '37', '35', '22', '34']))
+    video = YouTube('tgbNymZ7vqY')
 
     def test_best_format(self):
-        video = YouTube('tgbNymZ7vqY')
-        self.assertEqual(video._get_best_format(), '37')
+        self.assertEqual(YouTubeTests.video._get_best_format(), '37')
+
+    def test_formats(self):
+        self.assertEqual(YouTubeTests.video.formats, set(['5', '37', '35', '22', '34']))
+
+    def test_thumbnail(self):
+        self.assertNotEqual(YouTubeTests.video.thumbnail, None)
 
     def test_title(self):
-        video = YouTube('tgbNymZ7vqY')
-        self.assertEqual(video.title, 'The Muppets: Bohemian Rhapsody [Original Version]')
+        self.assertEqual(YouTubeTests.video.title, 'The Muppets: Bohemian Rhapsody [Original Version]')
+
+    def test_token(self):
+        self.assertNotEqual(YouTubeTests.video.token, None)
 
 
 
